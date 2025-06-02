9dbc75a0f2e94f18134b2fd0ac5e9cd80b85870f
aoife <aoife@10.125.83.140>
2025-06-02 13:47:01 -0400

    Merge branch 'main' of https://github.com/aoifecrow/CulminatingHangman.git into main

--- HangmanCulminating/src/luellaoife/textDriver.java
+++ HangmanCulminating/src/luellaoife/textDriver.java
@@ -1,8 +1,38 @@
 package luellaoife;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
 public class textDriver {
 
 	public static void main(String[] args) {
+		//read 
+		Random rand = new Random();
+		ArrayList<String> sentences = new ArrayList<>() ;
+		try { 
+			File f = new File("hangman_sentences_500_cleaned.txt");
+			Scanner in = new Scanner(f);
+			String content = "";
+			while (in.hasNext()) {
+				content += " " +in.next().replaceAll("'", "").replaceAll("\\."," ");
+			}
+			String[] sentencesArray = content.split(",");
+			
+			for (String s: sentencesArray) {
+				sentences.add(s);
+			}
+			
+			in.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("There must be a file issue");
+			System.out.println(e.getMessage());
+		}
+		
+		//randomly select sentence
+		String randomSentence = sentences.get(rand.nextInt(sentences.size()));
+		System.out.println(randomSentence);
 	}
 
 }
