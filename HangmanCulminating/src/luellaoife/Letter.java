45654765b9877532352567f4b4749eedbef19f38
lu_ellie <lu_ellie@localhost>
2025-06-02 13:22:20 -0400

    added letter class

---
+++ HangmanCulminating/src/luellaoife/Letter.java
@@ -0,0 +1,49 @@
+package luellaoife;
+
+/**
+ * Class to represent a single letter
+ * @author L.Mailloux
+ * @version 1.0
+ */
+
+public class Letter {
+	private char letter; 
+	private boolean guessed;
+	
+/**
+ * letter is made lowercase and not guessed by default
+ * @param value the character chosen
+ */
+	
+	public Letter (char value) {
+		this.letter = Character.toLowerCase(value);
+		guessed = false;
+	}	
+	
+/**
+ * 	returns the letter chosen
+ * @return the letter
+ */
+	public char getLetter() {
+		return letter;
+	}
+	
+/**
+ * returns letter if it has been guessed	
+ * @return true if guessed, elsewise false 
+ */
+	
+	public boolean isGuessed() {
+		return guessed; 
+	}
+	
+/**
+ * letter has been guessed	
+ */
+	
+	public void guess() {
+		guessed = true;
+	}
+	
+
+}
