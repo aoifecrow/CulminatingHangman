1636b3f22c605d41f8e66f24ffa7e6d83dca1ebb
lu_ellie <lu_ellie@localhost>
2025-06-02 13:21:04 -0400

    made letter class

--- HangmanCulminating/src/luellaoife/textDriver.java
+++ HangmanCulminating/src/luellaoife/textDriver.java
@@ -3,15 +3,6 @@ package luellaoife;
 public class textDriver {
 
 	public static void main(String[] args) {
-		//hello! bye
-		
-		
-		  
-		
-		for (int i=0; i <10; i++) {
-			System.out.println(10);
-		}
-
 	}
 
 }
